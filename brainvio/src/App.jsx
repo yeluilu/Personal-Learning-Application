cbc86ecf301234410087094ea4db96155696f7a6
Luis Castellanos <castellanosfluis@gmail.com>
2025-11-09 20:46:49 -0500

    deleting functions im not using

--- brainvio/src/App.jsx
+++ brainvio/src/App.jsx
@@ -1,5 +1,4 @@
-import Header from './Header.jsx'
-import Button from './Button.jsx'
+
 import './index.css'
 import React, { useState,useEffect } from 'react';
 
@@ -10,7 +9,7 @@ function App() {
 
   // Change body background to cover whole page
   useEffect(() => {
-    document.body.style.backgroundColor = isDarkMode ? '#0f0f1a' : '#f5f5f5';
+    document.body.style.backgroundColor = isDarkMode ? '#0f0f1a' : '#ffffffff';
   }, [isDarkMode]);
 
   const toggleMode = () => setIsDarkMode(!isDarkMode);
