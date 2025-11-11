3a033b43bed326755b7ebbd1d99feb03bd508d87
Luis Castellanos <castellanosfluis@gmail.com>
2025-11-11 17:50:46 -0500

    Added input for username and password

--- brainvio/src/Pages/LoginPage.jsx
+++ brainvio/src/Pages/LoginPage.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 
-
-function LoginPage(){
-    return (
-        <div className="container">
-        <h1 className="login-header">Login</h1>
-        </div>
+function LoginPage() {
+  return (
+    <div className="container">
+      <h1 className="login-header">Login</h1>
+      <form className="login-form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" className="login">Login</button>
+      </form>
+    </div>
   );
 }
 
-
-export default LoginPage
+export default LoginPage;
